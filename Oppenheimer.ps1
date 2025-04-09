8a87831688c3c4eea68a959736ed4d882830e0c7
Tachaeon <97981421+Tachaeon@users.noreply.github.com>
2025-04-09 08:15:24 -0400

    Update Oppenheimer.ps1

--- Oppenheimer.ps1
+++ Oppenheimer.ps1
@@ -22,7 +22,7 @@ $Boom = {
     $session.InvokeMethod($namespaceName, $instance, $methodName, $params)
 }
 
-$BoomFile = "$env:TEMP\Boom.ps1"
+$BoomFile = "$env:TEMP\Hi_Latacora.ps1"
 $Boom | Out-File -FilePath $BoomFile
 
 & $File -s -accepteula -nobanner powershell.exe -ExecutionPolicy Bypass -File $BoomFile
